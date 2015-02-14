b2d3dc67d83c97ea8354e30a49414c1316765d62
Junzhe Zhang <junzhez@junzhez.com>
2015-02-14 12:34:20 -0800

    Initial commit

---
+++ ImageRead/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace ImageRead
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string bitmapFilePath = @"J:\Github\ImageRead\lena.gif";
+            Bitmap c = new Bitmap(bitmapFilePath);
+            
+            int Nx1 = 10;
+            double Sigmax1 = 2;
+            int Nx2 = 10;
+            double Sigmax2 = 2;
+            double Theta1 = Math.PI/2;
+
+            int Ny1 = 10;
+            double Sigmay1 = 2;
+            int Ny2 = 10;
+            double Sigmay2 = 2;
+            double Theta2 = 0;
+
+            double alpha = 0.1;
+
+            Bitmap d = Program.Ind2Gray(c);
+
+            var mat = new DenseMatrix(d.Height, d.Width);
+
+            for (int i = 0; i < d.Height; i++)
+            {
+                for (int j = 0; j < d.Width; j++)
+                {
+                    mat.At(i, j, d.GetPixel(j, i).R);
+                }
+            }
+
+            DenseMatrix filter = Program.D2dGauss(Nx1, Sigmax1, Nx2, Sigmax2, Theta1);
+
+
+        }
+
+        static Convolute(DenseMatrix Mat, DenseMatrix Kernel)
+        {
+            int w = Mat.ColumnCount;
+            int h = Mat.RowCount;
+
+            int halfKernelWidth = kernelWidth / 2;
+            int halfKernelHeight = kernelHeight / 2;
+        }
+
+        static void ImageShow(Bitmap Img)
+        {
+            PictureBox P = new PictureBox
+            {
+                Size = Img.Size,
+            };
+
+            Form form = new Form
+            {
+                Size = Img.Size,
+            };
+
+            P.Image = Img;
+
+            form.Controls.Add(P);
+            form.Show();
+
+            Application.Run(form);
+        }
+
+        static Bitmap Ind2Gray(Bitmap Img)
+        {
+            int height = Img.Height;
+            int width = Img.Width;
+
+            Bitmap d = new Bitmap(width, height);
+
+            // Loop through the images pixels to reset color.
+            for (int x = 0; x < Img.Width; x++)
+            {
+                for (int y = 0; y < Img.Height; y++)
+                {
+                    Color pixelColor = Img.GetPixel(x, y);
+                    int grayScale = (int)((pixelColor.R * 0.3) + (pixelColor.G * 0.59) + (pixelColor.B * 0.11));
+                    Color newColor = Color.FromArgb(pixelColor.A, grayScale, grayScale, grayScale);
+                    d.SetPixel(x, y, newColor); // Now greyscale
+                }
+            }
+
+            return d;
+        }
+
+        static DenseMatrix D2dGauss(int N1, double Sigma1, int N2, double Sigma2, double Theta)
+        {
+            var r = new DenseMatrix(2, 2);
+
+            r.At(0, 0, Math.Cos(Theta));
+            r.At(0, 1, -Math.Sin(Theta));
+            r.At(1, 0, Math.Sin(Theta));
+            r.At(1, 1, Math.Cos(Theta));
+
+            var h = new DenseMatrix(N2, N1);
+
+            for (int i = 0; i < N2; i++)
+            {
+                for (int j = 0; j < N1; j++)
+                {
+                    var tmp = new DenseMatrix(2, 1);
+                    tmp.At(0, 0, j - (N1 + 1) / 2);
+                    tmp.At(1, 0, i - (N2 + 1) / 2);
+                    var u = r.Multiply(tmp);
+
+                    h.At(i, j, Program.Gauss(u.At(0, 0), Sigma1) * Program.Gauss(u.At(1, 0), Sigma2));
+                }
+            }
+
+            return h;
+        }
+
+        static double Gauss(double x, double std)
+        {
+            double y = Math.Exp(-Math.Pow(x, 2) / (2 * Math.Pow(std, 2))) / (std * Math.Sqrt(2 * Math.PI));
+
+            return y;
+        }
+
+        static double Dgauss(double x, double std)
+        {
+            double y = -x * Program.Gauss(x, std) / Math.Pow(std, 2);
+
+            return y;
+        }
+    }
+}
---
+++ ImageRead/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageRead")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageRead")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4aa6b3e3-aba6-40da-828b-285473451f76")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
